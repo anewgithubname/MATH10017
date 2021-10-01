6906d4704059b4ad4d5b0ac455c4854e45473fee
anewgithubname <song.liu@bristol.ac.uk>
2021-10-01 13:37:28 +0100

    added answer to the homework

---
+++ homework/sol.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+  // HW 3
+  printf("%d Fahrenheit = %.2f Celcius.\n", 10, (10-32.0)*5.0/9.0);
+  printf("%d Fahrenheit = %.2f Celcius.\n", 20, (20-32.0)*5.0/9.0);
+  printf("%d Fahrenheit = %.2f Celcius.\n", 30, (30-32.0)*5.0/9.0);
+  printf("%d Fahrenheit = %.2f Celcius.\n", 40, (40-32.0)*5.0/9.0);
+  printf("%d Fahrenheit = %.2f Celcius.\n", 50, (50-32.0)*5.0/9.0);
+  printf("%d Fahrenheit = %.2f Celcius.\n", 60, (60-32.0)*5.0/9.0);
+  printf("%d Fahrenheit = %.2f Celcius.\n", 70, (70-32.0)*5.0/9.0);
+  printf("%d Fahrenheit = %.2f Celcius.\n", 80, (80-32.0)*5.0/9.0);
+  printf("%d Fahrenheit = %.2f Celcius.\n", 90, (90-32.0)*5.0/9.0);
+  printf("%d Fahrenheit = %.2f Celcius.\n", 100, (100-32.0)*5.0/9.0);
+
+  // HW 4
+  printf("|\\\n");
+  printf("| \\\n");
+  printf("|  \\\n");
+  printf("|   \\\n");
+  printf("|____\\\n");
+
+}
